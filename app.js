5b7b567ae7f1f2b03fb13960fd46ca069267b17d
AzizBenIsmail <aziz.270700@gmail.com>
2025-04-14 16:00:03 +0100

    fix morgan for logs

--- app.js
+++ app.js
@@ -1,37 +1,38 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
-require('dotenv').config();
-const connectDB = require('./config/database');
-const authRouter = require('./routes/authRouter');
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+var logger = require("morgan");
 
-const http = require('http');
+require("dotenv").config();
+const http = require("http");
+const connectDB = require("./config/database");
 
+const authRouter = require("./routes/authRouter");
 
 const app = express();
-const port = process.env.PORT || 5000;
 
 // Connexion à MongoDB
-connectDB();    
+connectDB();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// Servir les fichiers statiques depuis le dossier public
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(logger("dev"));//combined
 
 // Routes
-app.use('/auth', authRouter);
+app.use("/auth", authRouter);
 
 // Route de base
-app.get('/', (req, res) => {
-  res.json({ message: 'Bienvenue sur l\'API Express!' });
+app.get("/", (req, res) => {
+  res.json({ message: "Bienvenue sur l'API Express!" });
 });
 
 // Démarrage du serveur HTTP
 const server = http.createServer(app);
-server.listen(port, () => {
-  console.log(`Le serveur est en cours d'exécution sur le port ${port}`);
-}); 
+server.listen(process.env.PORT, () => {
+  console.log(
+    `Le serveur est en cours d'exécution sur le port ${process.env.PORT}`
+  );
+});
