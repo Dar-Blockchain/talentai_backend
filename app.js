fa9eeefd1130cfdf6b9c1130fc1d501d5120f383
AzizBenIsmail <aziz.270700@gmail.com>
2025-04-14 15:11:38 +0100

    fix with http Protocol

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 require('dotenv').config();
 const connectDB = require('./config/database');
 const authRouter = require('./routes/authRouter');
+
+const http = require('http');
+
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 // Connexion à MongoDB
-connectDB();
+connectDB();    
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Servir les fichiers statiques depuis le dossier public
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.use('/auth', authRouter);
@@ -23,7 +30,8 @@ app.get('/', (req, res) => {
   res.json({ message: 'Bienvenue sur l\'API Express!' });
 });
 
-// Démarrage du serveur
-app.listen(port, () => {
+// Démarrage du serveur HTTP
+const server = http.createServer(app);
+server.listen(port, () => {
   console.log(`Le serveur est en cours d'exécution sur le port ${port}`);
 }); 
